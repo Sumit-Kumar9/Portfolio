396f1261566f208d4a6d1bfb6917ac7d2e720068
Sumit-Kumar9 <sumitkumar7061928607@gmail.com>
2023-12-18 20:56:51 +0530

    my first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ==================== Typing animation ================= */
+var typed = new Typed(".typing",{
+    strings:["Programmer","Software Developer","Web Developer","Database Designer","Content Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
